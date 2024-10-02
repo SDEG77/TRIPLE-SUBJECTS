9e64826bb3a4d04724bb467abf0d351244d3c719
Jann100815 <pescojohnanthony@gmail.com>
2024-10-02 16:42:23 +0800

    Done Landing Page with login/signup & add static route for login,signup,and index page.

--- routes/web.js
+++ routes/web.js
@@ -4,4 +4,13 @@ route.get('/', (req, res) => {
   res.render('index');
 });
 
+route.get('/login', (req, res) => {
+  res.render('login');
+});
+
+route.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+
 module.exports = route;
