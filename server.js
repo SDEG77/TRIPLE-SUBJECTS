a08605aef5172da2cc645f6ac03df28128e80a79
SDEG77 <hahahayeahmannn@gmail.com>
2024-10-03 17:33:33 +0800

    Integrated Offline Tailwind

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const web = require('./routes/web');
 const PORT = 6969;
 
 app.listen(PORT, () => {
-  console.log(`SERVER RUNNING ON PORT: localhost:${PORT}/`);
+  console.log(`SERVER RUNNING ON PORT: [http://localhost:${PORT}/]`);
 });
 
 app.use(express.static('public'));
