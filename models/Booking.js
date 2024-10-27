023da734ae7c70571a66afa838eff10c770a4df4
Ark <istorya707@gmail.com>
2024-10-27 22:58:59 +0800

    Merge branch 'master' of https://github.com/SDEG77/TRIPLE-SUBJECTS into bayan

--- models/Booking.js
+++ models/Booking.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = mongoose.Schema({
-  client_id: String, // Reference to User model
+  client_id: String, 
   receipt_uploaded: String,
   package: String,
   service: String,
